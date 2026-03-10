620673cda19b8c80aee59237bd39f8479c9b56d1
Haxxolotto <62444678+Haxxxxxx@users.noreply.github.com>
2026-03-10 16:25:14 +0100

    feat: add native deepMerge utility to replace lodash/merge

---
+++ src/utils/deepMerge.js
@@ -0,0 +1,25 @@
+/**
+ * Deep merge objects. Later sources override earlier ones.
+ * Arrays are replaced, not concatenated (matches lodash/merge behavior for this codebase).
+ */
+export default function deepMerge(target, ...sources) {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key of Object.keys(source)) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        deepMerge(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return deepMerge(target, ...sources);
+}
+
+function isObject(item) {
+  return item && typeof item === 'object' && !Array.isArray(item);
+}
